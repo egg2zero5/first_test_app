5f7bf6fc75a57b39ca4ff629512ba02ddf1e0365
ahliang <liangmmu09@gmail.com>
2011-03-09 00:45:46 +0800

    Environment Editing

--- config/environment.rb
+++ config/environment.rb
@@ -5,18 +5,4 @@ require File.expand_path('../application', __FILE__)
 Depotnew::Application.initialize!
 
 
-#=begin
-Depotnew::Application.configure do
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-:address => "smtp.gmail.com",
-:port => 587,
-:domain => "domain.of.sender.net",
-:authentication => "plain",
-:user_name => "dave",
-:password => "secret",
-:enable_starttls_auto => true
-}
-end
 
-#=end
