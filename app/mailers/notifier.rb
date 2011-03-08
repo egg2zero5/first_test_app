b38c441aa8774f7a44164b60da16a0bd47a278a5
ahliang <liangmmu09@gmail.com>
2011-03-09 01:25:57 +0800

    Sending main Beta

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -7,7 +7,7 @@
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
 class Notifier < ActionMailer::Base
-  default :from => 'Sam Ruby <depot@example.com>'
+  default :from => 'Railson <railson2020@gmail.com>'
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
